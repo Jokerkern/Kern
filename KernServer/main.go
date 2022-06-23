54d80c2d8fba35ad487e84767040fad6ef181ba2
Kern <852946650@qq.com>
2022-06-23 22:16:54 +0800

    https

--- KernServer/main.go
+++ KernServer/main.go
@@ -5,7 +5,7 @@ import "github.com/kataras/iris/v12"
 func main() {
 	app := iris.Default()
 	// TLS 使用文件例子
-	err := app.Run(iris.TLS("127.0.0.1:8080", "www.jokerkern.cn_bundle.crt", "www.jokerkern.cn.key"))
+	err := app.Run(iris.TLS("127.0.0.1:443", "www.jokerkern.cn_bundle.crt", "www.jokerkern.cn.key"))
 	if err != nil {
 		return
 	}
